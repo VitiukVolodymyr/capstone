ee6d306702ebf2601fc2a508b1550b9eefd5a331
VolodymyrVitiuk <v.volodymyr@wwg.it>
2025-03-11 18:58:56 +0100

    added addToCart feature

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { CartProvider } from './contexts/cart.context';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
 import './index.scss';
-import { ProductsProvider } from './contexts/products.context';
-import { CartContext, CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
