645ba55005368d1cfa962d66bec5735f8c5d1d90
VolodymyrVitiuk <v.volodymyr@wwg.it>
2025-03-11 20:32:43 +0100

    added checkoutItem component

--- src/components/cart-dropdown/cart-dropdown.componen.jsx
+++ src/components/cart-dropdown/cart-dropdown.componen.jsx
@@ -3,9 +3,17 @@ import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
+import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = isCartOpen => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, setIsCartOpen } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+    setIsCartOpen(prev => !prev);
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -13,7 +21,7 @@ const CartDropdown = isCartOpen => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
