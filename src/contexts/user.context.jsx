31d2ceba83d19b33fa4be0a4ae1e5548151b5209
VolodymyrVitiuk <v.volodymyr@wwg.it>
2025-05-13 19:55:02 +0200

    added useReducer to cartContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,6 +3,7 @@ import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
   currentUser: null,
@@ -17,7 +18,7 @@ const userReducer = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case 'SET_CURRENT_USER':
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
       return { ...state, currentUser: payload };
 
     default:
@@ -33,7 +34,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = user => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
