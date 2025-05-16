6517f9bcbebfd6beef1fb6e921f1dc042ca93bee
VolodymyrVitiuk <v.volodymyr@wwg.it>
2025-05-16 15:59:52 +0200

    refactor user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,12 +4,13 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.componen';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { CartContext } from '../../contexts/cart.context';
-import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { LogoContainer, NavLink, NavLinks, NavigationContainer } from './navigation.styles';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+// import logger from 'redux-logger';
+import { rootReducer } from './root-reducer';
+
+const loggerMiddleware = store => next => action => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type', action.type);
+  console.log('payload', action.payload);
+  console.log('current state', store.getState());
+  next(action);
+  console.log('next state', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
+const composeEnchancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composeEnchancers);
